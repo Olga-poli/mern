74a01d5b8696dc430e3d2ed3a94eb383a64d5425
Olha Polishko <polishko.official@gmail.com>
2022-11-07 19:06:16 +0200

    login & jwt added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,23 +1,27 @@
-// const { RESPONSE_MESSAGE } = import('../constants');
+// const { RESPONSE_MESSAGE } = require('../constants');
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+const config = require('../config/default.json');
 const router = Router();
 
 const RESPONSE_MESSAGE = {
   ERROR: {
     IS_EMAIL: 'Must be email',
     PASSWORD_LENGTH: 'Must be longer then 1 symbol',
-    INCORRECT_DATA: 'Incorrect login data',
+    PASSWORD_EXIST: 'Enter password',
+    INCORRECT_DATA: 'Incorrect input data',
+    INCORRECT_PASSWORD: 'Incorrect password',
     USER_ALREADY_EXIST: 'Such user already exist',
+    USER_DOESNT_EXIST: 'User doesn\t exist',
     SERVER_ERROR: 'Something gone wrong, try again',
   },
   SUCCESS: {
     USER_CREATED: 'User successfully created',
   }
 };
-
 
 // /api/auth/register
 router.post('/register',
@@ -52,8 +56,42 @@ router.post('/register',
 });
 
 // /api/auth/login
-router.post('/login', async (request, response) => {
+router.post('/login',
+  [
+    check('email', RESPONSE_MESSAGE.ERROR.IS_EMAIL).normalizeEmail().isEmail,
+    check('password', RESPONSE_MESSAGE.ERROR.PASSWORD_EXIST).exists(),
+  ],
+  async (request, response) => {
+  try {
+    const validationErrors = validationResult(request);
+    if (!validationErrors.isEmpty()) {
+      return response.status(400).json({ errors: validationErrors.array(), message: RESPONSE_MESSAGE.ERROR.INCORRECT_DATA });
+    }
 
+    const {email, password} = request.body;
+    const user = User.findOne({ email });
+
+    if (!user) {
+      return response.status(400).json({ message: RESPONSE_MESSAGE.ERROR.USER_DOESNT_EXIST});
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return response.status(400).json({ message: RESPONSE_MESSAGE.ERROR.INCORRECT_PASSWORD });
+    }
+
+    const token = jwt.sign(
+      {userId: user.id},
+      config.get('secretKey'),
+      { expiresIn: '1h' }
+    );
+
+    return response.json({ token, userId: user.id });
+
+  } catch (error) {
+    response.status(500).json({message: RESPONSE_MESSAGE.ERROR.SERVER_ERROR})
+  }
 });
 
 module.exports = router;
